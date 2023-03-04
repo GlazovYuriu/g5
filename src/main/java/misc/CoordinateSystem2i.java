8293446badfec199a83b4771bddd62a340896f97
andreevpa.24 <Pavelandreev2006@gmail.com>
2023-03-04 10:14:11 +0300

    json works

--- src/main/java/misc/CoordinateSystem2i.java
+++ src/main/java/misc/CoordinateSystem2i.java
@@ -1,5 +1,8 @@
 package misc;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.humbleui.skija.RRect;
 import io.github.humbleui.skija.Rect;
 
@@ -21,6 +24,16 @@ public class CoordinateSystem2i {
      * размер СК
      */
     private final Vector2i size;
+    /**
+     * Конструктор ограниченной двумерной целочисленной системы координат
+     *
+     * @param min минимальные координаты
+     * @param max максимальные координаты
+     */
+    @JsonCreator
+    public CoordinateSystem2i(@JsonProperty("min") Vector2i min, @JsonProperty("max") Vector2i max) {
+        this(min.x, min.y, max.x - min.x, max.y - min.x);
+    }
 
     /**
      * Конструктор ограниченной двумерной целочисленной системы координат
@@ -52,10 +65,10 @@ public class CoordinateSystem2i {
      *
      * @return случайные координаты внутри СК
      */
+    @JsonIgnore
     public Vector2i getRandomCoords() {
         return Vector2i.rand(min, max);
     }
-
     /**
      * Возвращает относительное положение вектора в СК
      *
@@ -129,6 +142,7 @@ public class CoordinateSystem2i {
      *
      * @return размер СК
      */
+    @JsonIgnore
     public Vector2i getSize() {
         return size;
     }
